d93ee0fa6ee6a25d324dca6c6b5da58bcb6f0667
Showy1126 <156112006+Showy1126@users.noreply.github.com>
2025-01-06 11:27:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
+    <title>Este es el inicio broo</title>
 </head>
 <body>
     <form action="logica.php" method="POST">
