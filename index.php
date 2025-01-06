ea2cfaac2b7a4fd184efca2f15c999aa7e61114e
Showy1126 <kevinsalinasnacun@gmail.com>
2025-01-06 13:37:55 -0500

    Se agregaron estilo

--- index.php
+++ index.php
@@ -4,24 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Este es el index</title>
+    <link rel="stylesheet" href="./css/estiloIndex.css">
 </head>
 <body>
+    <main>
     <form action="logica.php" method="POST">
         <h1>Formulario de registro</h1>
         <span>Nombre</span><br>
-        <input type="text" name="nombre" required><br>
+        <input type="text" name="nombre" class="texto" required><br>
         <span>Apellido</span><br>
-        <input type="text" name="apellido" required><br>
+        <input type="text" name="apellido"  class="texto" required><br>
         <span>Telefono</span><br>
-        <input type="number" name="telefono" required><br>
+        <input type="number" name="telefono"  class="texto" required><br>
         <span>Direccion</span><br>
-        <input type="text" name="direccion" required><br>
+        <input type="text" name="direccion"  class="texto" required><br>
         <span>Genero</span><br>
         <select name="genero">
             <option value="masculino">Masculino</option>
             <option value="femenino">Femenino</option>
         </select><br><br>
-        <input type="submit">
+        <div class="boton-container">
+            <input type="submit">
+        </div>
     </form>
+    </main>
 </body>
 </html>
