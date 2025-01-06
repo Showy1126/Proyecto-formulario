2e286022f7c9d794d26b1caa91d1e9aaab5ee4d9
Showy1126 <kevinsalinasnacun@gmail.com>
2025-01-06 12:22:42 -0500

    Se hizo una modificaciona inicio.php y  se creo el archivo de estilos para inicio.php

--- inicio.php
+++ inicio.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/estiloInicio.css">
 </head>
 <body>
-    <h1>Pagina de inicio</h1>
-    <a href="index.php">Formulario de registro</a>
+    <main>
+        <h1>Pagina de inicio</h1>
+        <a href="index.php">Formulario de registro</a>
+    </main>
 </body>
 </html>
