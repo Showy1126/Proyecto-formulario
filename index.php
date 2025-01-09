3e569b54e54b31b5a584e52bf1d54504237303be
Showy1126 <kevinsalinasnacun@gmail.com>
2025-01-08 21:34:33 -0500

    Se le agregaron estilos a logica.php

--- index.php
+++ index.php
@@ -1,38 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Este  Ya no es el index</title>
+    <title>Este Ya no es el index</title>
     <link rel="stylesheet" href="./css/estiloIndex.css">
     <link rel="stylesheet" href="./css/bulma.min.css">
 </head>
+
 <body>
     <main>
-    <div class="container">
-  <div class="notification is-primary">
-    This container is <strong>centered</strong> on desktop and larger viewports.
-  </div>
-</div>
-    <form action="logica.php" method="POST">
-        <h1>Formulario de registro</h1>
-        <span>Nombre</span><br>
-        <input type="text" name="nombre" class="texto" required><br>
-        <span>Apellido</span><br>
-        <input type="text" name="apellido"  class="texto" required><br>
-        <span>Telefono</span><br>
-        <input type="number" name="telefono"  class="texto" required><br>
-        <span>Direccion</span><br>
-        <input type="text" name="direccion"  class="texto" required><br>
-        <span>Genero</span><br>
-        <select name="genero">
-            <option value="masculino">Masculino</option>
-            <option value="femenino">Femenino</option>
-        </select><br><br>
-        <div class="boton-container">
-        <button class="button is-success is-light">Success</button>
+        <div class="container">
+            <div class="notification is-primary">
+                Este <strong>formulario</strong> es muy importante.
+            </div>
         </div>
-    </form>
+        <form action="logica.php" method="POST">
+            <h1>Formulario de registro</h1>
+            <span>Nombre</span><br>
+            <input type="text" name="nombre" class="texto" required><br>
+            <span>Apellido</span><br>
+            <input type="text" name="apellido" class="texto" required><br>
+            <span>Telefono</span><br>
+            <input type="number" name="telefono" class="texto" required><br>
+            <span>Direccion</span><br>
+            <input type="text" name="direccion" class="texto" required><br>
+            <span>Genero</span><br>
+            <select name="genero">
+                <option value="masculino">Masculino</option>
+                <option value="femenino">Femenino</option>
+            </select><br><br>
+            <div class="boton-container">
+                <button class="button is-success is-light">Success</button>
+            </div>
+        </form>
     </main>
 </body>
+
 </html>
--- logica.php
+++ logica.php
@@ -4,9 +4,25 @@ $apellido = $_POST["apellido"];
 $telefono = $_POST["telefono"];
 $direccion = $_POST["direccion"];
 $genero = $_POST["genero"];
+?>
 
-echo "Nombre: ".$nombre."<br>";
-echo "Apellido: ".$apellido."<br>";
-echo "Telefono: ".$telefono."<br>";
-echo "Direccion: ".$direccion."<br>";
-echo "Genero: ".$genero."<br>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logica</title>
+    <link rel="stylesheet" href="./css/bulma.min.css">
+    <link rel="stylesheet" href="./css/estiloLogica.css">
+</head>
+<body>
+    <main>
+        <div class="box has-background-success"><?= "Nombre: $nombre" ?></div>
+        <div class="box has-background-danger"><?= "Apellido: $apellido" ?></div>
+        <div class="box has-background-warning"><?= "Telefono: $telefono" ?></div>
+        <div class="box has-background-info"><?= "Direccion: $direccion" ?></div>
+        <div class="box has-background-dark has-text-white-ter"><?= "Genero: $genero" ?></div>
+    </main>
+    
+</body>
+</html>
