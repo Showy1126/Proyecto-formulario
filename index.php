7dcd108f338cf850f7e45189870ab1f28651440b
Showy1126 <kevinsalinasnacun@gmail.com>
2025-01-08 21:11:14 -0500

    Se modifico el index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Este es el index</title>
+    <title>Este  Ya no es el index</title>
     <link rel="stylesheet" href="./css/estiloIndex.css">
     <link rel="stylesheet" href="./css/bulma.min.css">
 </head>
